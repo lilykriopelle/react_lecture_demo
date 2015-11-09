d1e250d0b037f3e2035ce1ea629e2608b8838209
Lily Riopelle <lilykriopelle@gmail.com>
2015-11-08 19:04:26 -0500

    add demo skeleton

--- click_counter.js
+++ click_counter.js
@@ -1,30 +1,16 @@
 var ClickCounter = React.createClass({
   getInitialState: function() {
-    return {clicks: 0};
   },
 
   increment: function() {
-    this.setState({clicks: this.state.clicks + 1});
   },
 
   render: function() {
-    return <div className="click-counter">
-      <h2>Click Counter Number {this.props.number}</h2>
-      <p>{this.state.clicks}</p>
-      <button onClick={this.increment}>CLICK ME</button>
-    </div>;
+
   }
 });
 
 var ManyCounters = React.createClass({
   render: function() {
-    var counters = [];
-    for (var i = 0; i < this.props.numCounters; i ++) {
-      counters.push(<ClickCounter number={i} />);
-    }
-
-    return <div>
-            {counters}
-           </div>;
   }
 });
